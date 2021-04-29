ab512f68f4f912cfe4acadfe0c185a8ddc53dbeb
brendateodoro <teo19002@byui.edu>
2021-04-28 23:28:31 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 document.querySelector("#lastmod").textContent=document.lastModified;
-document.querySelector("#lasyear")
+
+
+document.querySelector("#theyear").textContent = new Date().getFullYear();
