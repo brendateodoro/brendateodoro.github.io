94b41ebdc28f18fd4dcb4fd0dd780f0de6f98125
brendateodoro <teo19002@byui.edu>
2021-05-11 17:57:30 -0300

    commit

--- lesson3/js/index.js
+++ lesson3/js/index.js
@@ -1,6 +1,6 @@
 document.querySelector("#lastmod").textContent=document.lastModified;
 document.querySelector("#theyear").textContent = new Date().getFullYear();
 function toggleMenu(){
-  console.log(document.getElementById("primaryNav").classList);
+  
  document.getElementById("primaryNav").classList.toggle("hide");
 }
