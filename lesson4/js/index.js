825c40188bbcef3c71552c3c971a3375ddc36d87
brendateodoro <teo19002@byui.edu>
2021-05-15 22:31:15 -0300

    commit

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -4,3 +4,8 @@ function toggleMenu(){
   
  document.getElementById("primaryNav").classList.toggle("hide");
 }
+const datefield = document.querySelector("date");
+const now = new Date();
+datefield.textContent = now;
+const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle:"full" }).format(now);
+datefield.innerHTML = '<em>${fulldate}</em>';
