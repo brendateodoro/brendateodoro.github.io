37941da5b21f52e13c0be11574dd8ea7c4e5e650
brendateodoro <teo19002@byui.edu>
2021-05-15 22:38:54 -0300

    commit

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,11 +1,12 @@
-document.querySelector("#lastmod").textContent=document.lastModified;
-document.querySelector("#theyear").textContent = new Date().getFullYear();
+
+
 function toggleMenu(){
   
  document.getElementById("primaryNav").classList.toggle("hide");
 }
-const datefield = document.querySelector("date");
+
+const datefield = document.querySelector("#date");
 const now = new Date();
-datefield.textContent = now;
+
 const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle:"full" }).format(now);
-datefield.innerHTML = '<em>${fulldate}</em>';
+datefield.innerHTML = `<em>${fulldate}</em>`;
