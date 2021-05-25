74376aadb330e8c523d0fa379fe5165168ac54b2
brendateodoro <teo19002@byui.edu>
2021-05-25 17:54:51 -0300

    commit

--- lesson5/js/index.js
+++ lesson5/js/index.js
@@ -17,4 +17,20 @@ function toggleMenu(){
  const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle:"full" }).format(now);
  datefield.innerHTML = `<em>${fulldate}</em>`;
 
+ function check() {
+  var now = new Date();
+  var month = now.getMonth();
+  var dayOfMonth = now.getDate();
+  var dayOfWeek = now.getDay();
+  let output = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+
+  if (dayOfWeek == 6 ) {   //0 is Sunday, 6 is Saturday
+   output = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+
+} else {
+     
+  }
+  
+  document.getElementById("output").innerHTML = output;
+}
  
