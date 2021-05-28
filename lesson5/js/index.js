04c48f9af6de8b2c967cbdc46e6662de0724dd4f
brendateodoro <teo19002@byui.edu>
2021-05-27 23:46:43 -0300

    commit

--- lesson5/js/index.js
+++ lesson5/js/index.js
@@ -24,7 +24,7 @@ function toggleMenu(){
   var dayOfWeek = now.getDay();
   let output = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
 
-  if (dayOfWeek == 6 ) {   //0 is Sunday, 6 is Saturday
+  if (dayOfWeek == 3 ) {   //0 is Sunday, 6 is Saturday
    output = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
 
 } else {
