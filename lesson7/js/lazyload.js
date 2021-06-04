c9f35072358f67662b3fbe0ca8b11647c8a7c2ff
brendateodoro <teo19002@byui.edu>
2021-06-04 17:34:38 -0300

    commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,11 +1,17 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+
+const imgOptions = {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 1
+};
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
-
 
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
@@ -26,6 +32,3 @@ if('IntersectionObserver' in window) {
 }
 
 
-imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
