aa759158248e23a56ecf4e328afc576e812f570d
brendateodoro <teo19002@byui.edu>
2021-06-04 16:48:47 -0300

    commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -7,7 +7,23 @@ const loadImages = (image) => {
 };
 
 
-
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
 
 
 imagesToLoad.forEach((img) => {
