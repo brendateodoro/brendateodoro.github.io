395e2abad7992f8679180131bf069c1b670658b3
brendateodoro <teo19002@byui.edu>
2021-06-11 12:55:28 -0300

    commit

---
+++ lesson8/js/stormcenter.js
@@ -0,0 +1,44 @@
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+  button.addEventListener('click', function(){
+    buttons.forEach (btn=>btn.classList.remove("active"));
+    this.classList.add('active');
+  })
+})
+function toggleMenu(){
+  
+  document.getElementById("primaryNav").classList.toggle("hide");
+ }
+ const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+
+const imgOptions = {
+  rootMargin: "0px 0px 50px 0px",
+  threshold: 1
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  },imgOptions);
+  
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
