ccee16680f6691aef42a58dd552265a28c0552d8
brendateodoro <teo19002@byui.edu>
2021-06-17 18:11:49 -0300

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -9,12 +9,20 @@ fetch(requestURL)
  for (let i = 0; i < prophets.length; i++ ) {
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
+  let p = document.createElement("p");
+  let place = document.createElement("place");
   let img = document.createElement("img");
-h2.innerHTML = `${prophets[i].name} <span class="purple">${prophets[i].lastname}</span>`;
+
+h2.innerHTML = `<span class="purple"> ${prophets[i].name} ${prophets[i].lastname}</span>`;
 img.setAttribute('src', prophets[i].imageurl);
 img.setAttribute("alt", "The official portrait ${prophets[i].name} $prophets[i].lastname}!");
+p.innerHTML = 'Date of Birth: ' + `${prophets[i].birthdate}`;
+place.innerHTML = 'Place of Birth: ' + `${prophets[i].birthplace}`;
   card.append(h2);
+  card.append(p);
+  card.append(place);
   card.append(img);
+
   document.querySelector('div.cards').appendChild(card);
 }
   });
