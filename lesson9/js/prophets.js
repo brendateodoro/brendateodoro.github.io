d0c416375790ddeeb668d350d05f487fea321cb8
brendateodoro <teo19002@byui.edu>
2021-06-17 17:26:14 -0300

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -4,16 +4,17 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+   //   console.table(jsonObject);  // temporary checking for valid response and data parsing
  const prophets = jsonObject['prophets'];
  for (let i = 0; i < prophets.length; i++ ) {
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
-  
-  h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-  
-  card.appendChild(h2);
-  
+  let img = document.createElement("img");
+h2.innerHTML = `${prophets[i].name} <span class="purple">${prophets[i].lastname}</span>`;
+img.setAttribute('src', prophets[i].imageurl);
+img.setAttribute("alt", "The official portrait ${prophets[i].name} $prophets[i].lastname}!");
+  card.append(h2);
+  card.append(img);
   document.querySelector('div.cards').appendChild(card);
 }
   });
