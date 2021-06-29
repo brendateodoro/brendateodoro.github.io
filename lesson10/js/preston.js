605b84e1b1c78313b060493d8eb5908a4f806e5d
brendateodoro <teo19002@byui.edu>
2021-06-29 13:24:13 -0300

    commit

--- lesson10/js/preston.js
+++ lesson10/js/preston.js
@@ -45,3 +45,28 @@ function windChill(t, s){
   let windChill = (35.74 + .6215 * t -  35.75 * Math.pow(s , .16 ) + .4275 * t * Math.pow(s , .16));
   return windChill;
 }
+const cityid="5604473";
+const APPID ="5f872e38bc4df7e78e9ab0f612fa0eb9";
+
+const apiURL=`https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    // console.log(jsObject);
+    const temperature = document.querySelector('#temperature');
+  temperature.textContent = jsObject.main.temp.toFixed(0);
+    const currently = document.querySelector('#currently');
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    currently.innerHTML = `<strong>${desc.toUpperCase()}</strong>`;
+
+
+
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;  // note the concatenation
+
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc.toUpperCase);
+   
+  });
