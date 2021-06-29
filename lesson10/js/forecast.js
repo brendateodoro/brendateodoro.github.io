5153f43f543dbce727759cd56fabdc62fbe76ae4
brendateodoro <teo19002@byui.edu>
2021-06-29 07:19:32 -0300

    commit

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,25 +1,13 @@
 const cityid="5604473";
 const APPID ="5f872e38bc4df7e78e9ab0f612fa0eb9";
 
-const apiURL=`https://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
+const apiURL=`https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&APPID=${APPID}&units=imperial`;
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    // console.log(jsObject);
-    const temperature = document.querySelector('#temperature');
-  temperature.textContent = jsObject.main.temp.toFixed(0);
-    const currently = document.querySelector('#currently');
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    currently.innerHTML = `<strong>${desc.toUpperCase()}</strong>`;
-
-
-
-
-    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;  // note the concatenation
-
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc.toUpperCase);
-   
+console.log(jsObject);
+let day = 0;
+const dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const fivedayforecast =  
   });
