a34fe0bf58d2db8bcd110c31e32dbb063dd0e45b
brendateodoro <teo19002@byui.edu>
2021-06-29 08:32:38 -0300

    commit

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -11,11 +11,16 @@ let day = 0;
 const dayofWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 const fiveday =  jsObject.list.filter ( forecast => forecast.dt_txt.includes('12:00:00'));
 console.log(fiveday);
-fiveday.forEach( x=>{
+fiveday.forEach( x => {
   let d = new Date(x.dt_txt);
-  // console.log(d);
+ //console.log(d);
   document.getElementById(`dayofWeek${day+1}`).textContent = dayofWeek[d.getDay()];
   document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
   day++
 })
+const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;  // note the concatenation
+
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc.toUpperCase);
   });
